d211b902c13d93b165fa1305a96acfa750be5f04
Emmanuel Carmona <pinombole@gmail.com>
2021-08-10 13:48:24 -0300

    Desafio 2 Clase 3

---
+++ src/componentes/NavBar.js
@@ -0,0 +1,14 @@
+const NavBar = () => {
+    return (
+        <>
+            <h1>Navbar por Componente React</h1>
+            <div class="container">
+                <a class="btn btn-primary mx-1" href="#" role="button">Link 1</a>
+                <a class="btn btn-primary mx-1" href="#" role="button">Link 2</a>
+                <a class="btn btn-primary mx-1" href="#" role="button">Link 3</a>
+            </div>
+        </>
+    )
+}
+
+export default NavBar;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//Carga la variable React en Scope
+import React from "react"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//Carga la variable ReactDOM en Scope
+import ReactDom from "react-dom"
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//Agrega un componente React
+import App from "./App"
+
+//Agrega una función render corriendo
+ReactDom.render(<App />, document.querySelector("#root"))
